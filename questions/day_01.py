81379eb4d3b76f9a8f1c2e96a5e91b2bf1868399
hdxestudart <erico.studart@hashdex.com>
2025-06-01 10:20:30 -0300

    feat: day one interview questions

---
+++ questions/day_01.py
@@ -0,0 +1,38 @@
+"""
+Question 1:
+What are the key features of Python?
+
+1. Easy to learn
+2. Easy syntax
+3. Dinamically typed
+4. Object oriented (OOP)
+5. Extensive library
+6. Strong community support
+"""
+
+"""
+Question 2:
+What are the differences between Python 2 and Python 3?
+
+
+"""
+
+"""
+Question 3:
+What are python data types?
+
+Python has several built-in data types, such as:
+
+Numerics: int, float, complex
+Text: string 
+Dictionaries: Those are structures that are used to map data.
+Lists: This is an ordered structure in which multiple typed objects can be stored => multable
+Tuples: This is another ordered structured but this time, the data is unmutable within the tuple.
+Boolean: A binary represented data type, eg. True or False
+"""
+
+"""
+Question 4:
+How to create a function in Python?
+
+"""
